bc6eca6d65318d13cd458d3df854e7e0a474f22d
Massive12356 <edwardmintah17@gmail.com>
2025-10-13 12:00:06 +0000

    new changes made but not completed yet

--- src/services/adminService.ts
+++ src/services/adminService.ts
@@ -110,6 +110,18 @@ class AdminService {
       throw new Error('Failed to fetch users');
     }
   }
+
+  async getUsers(){
+    try {
+      const response = await apiClient.get("/user/all-users");
+      console.log("[adminService]RESPONSE FROM BACKEND:", response.data.users)
+      return response.data.users
+    } catch (error) {
+      const err = error as AxiosError<{message?: string}>
+      console.log('[adminService] ERROR RESPONSE:', err.response?.data || err.message);
+      throw new Error(err.response?.data?.message || '[adminService] Failed to Fetch Users');
+    }
+  }
 }
 
 export const adminService = new AdminService();
